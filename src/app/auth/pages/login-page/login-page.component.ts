16ec73d4f9e684ca2d7c6bd417413e6732b04440
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-23 16:37:22 +0200

    add: 👔 auth service part 2 - login & local storage

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 
+import { AuthService } from '@auth/services/auth.service';
+
 @Component({
   selector: 'app-login-page',
   imports: [RouterLink, ReactiveFormsModule],
@@ -11,6 +13,8 @@ export class LoginPageComponent {
   fb = inject(FormBuilder);
   hasError = signal(false);
   isPosting = signal(false);
+
+  authService = inject(AuthService);
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -28,6 +32,9 @@ export class LoginPageComponent {
     }
 
     const { email = '', password = '' } = this.loginForm.value;
-    console.log({ email, password });
+
+    this.authService.login(email!, password!).subscribe((resp) => {
+      console.log(resp);
+    })
   }
 }
