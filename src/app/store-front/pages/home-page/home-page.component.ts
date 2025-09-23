f9984405b4851c26968a903c758932018d8e4b3a
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-23 12:54:39 +0200

    add: ✨ pagination - with rxResource

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
@@ -13,11 +15,24 @@ import { PaginationComponent } from "@shared/components/pagination/pagination.co
 })
 export class HomePageComponent {
   productsService = inject(ProductsService);
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map(params => (params.get('page') ? +params.get('page')! : 1)),
+      map(page => (isNaN(page) ? 1 : page))
+    ),
+    {
+      initialValue: 1
+    }
+  )
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.currentPage() - 1 }),
     stream: ({ params }) => {
-      return this.productsService.getProducts({});
-    }
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+      });
+    },
   })
 }
